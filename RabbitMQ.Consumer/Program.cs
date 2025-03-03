2257f9ef653a59d73ee7e05ac0175469fb6d7669
Hasan Eren Akgöz <hasanerenakgz@gmail.com>
2025-03-04 00:02:32 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqps://rhxapzxa:auD3cHM705kex3dbH2TdAZfDhuZfEPTq@cow.rmq2.cloudamqp.com/rhxapzxa")
+};
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "my-queue", durable: true, exclusive: false, autoDelete: true, arguments: null);
+
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "my-queue", autoAck: true, consumer: consumer);
+consumer.Received += (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Create a connection
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqps://rhxapzxa:auD3cHM705kex3dbH2TdAZfDhuZfEPTq@cow.rmq2.cloudamqp.com/rhxapzxa")
+};
+
+// Create a channel
+using IConnection connection =  connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Create a queue
+
+channel.QueueDeclare(queue :"my-queue", durable: true, exclusive: false, autoDelete: true, arguments: null);
+
+// Send a message the queue
+
+// RabitMQ accepts messages of type Byte for queuing.
+
+//byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello, World!");
+//channel.BasicPublish(exchange: "", routingKey: "my-queue", basicProperties: null, body: messageBodyBytes);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Hello, World! {i}");
+    channel.BasicPublish(exchange: "", routingKey: "my-queue", basicProperties: null, body: messageBodyBytes);
+}
+Console.Read();
+
